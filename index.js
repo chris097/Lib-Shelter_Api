b8ae82e3395f431efd0427814df7ecb49a395002
chris097 <chrisfidel.international@gmail.com>
2020-11-20 13:23:05 +0100

    add POST method

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ app.get('/api/items/:id', (req, res) =>{
 
 //POST
 app.post('/api/items', (req, res) => {
+    const {error} = validateCourse(req.body)
+
+    if(error) return res.status(400).send(error.details[0].message)
+
     const item = {
         id: Items.length +1,
         name: req.body.name
@@ -36,7 +40,42 @@ app.post('/api/items', (req, res) => {
     res.send(item)
 })
 
+app.put('/api/items/:id', (req, res) => {
+    //Look up the Items
+    const item = Items.find(c => c.id === parseInt(req.params.id))
+    //If not existing, return 404
+    if(!item) res.status(404).send('The items of the given ID was not found...')
+    //validate
+    const {error} = validateCourse(req.body)
+    //If invalid, return 400 -Bad request
+    if(error) return res.status(400).send(error.details[0].message)
+    //Update Items
+    item.name =req.body.name
+    //Return the update Items
+    res.send(item)
+})
+
+app.delete('/api/items/:id', (req, res) => {
+      //Look up the Items
+      const item = Items.find(c => c.id === parseInt(req.params.id))
+      //If not existing, return 404
+      if(!item) res.status(404).send('The items of the given ID was not found...')
+      //Delete
+      const index = Items.indexOf(item)
+      Items.splice(index, 1)
+      //Return the same item
+      res.send(item)
+})
+
+function validateCourse(course) { 
+    //Validate
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+    return Joi.validate(course, schema)
+}
 
 
+//Environment Variable
 const port = process.env.PORT || 4040;
 app.listen(port, console.log(`Listening on port ${port}...`))
