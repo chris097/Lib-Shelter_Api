37ac70f7abdbaa058c74ebadb77127ae19586048
chris097 <chrisfidel.international@gmail.com>
2020-11-23 12:33:30 +0100

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('express-async-errors')
 const express = require("express");
 const winston = require('winston')
+const cors = require('cors')
 const app = express();
 
+app.use(cors())
 require('./startup/logging')()
 require('./startup/route')(app)
 require('./startup/db')();
