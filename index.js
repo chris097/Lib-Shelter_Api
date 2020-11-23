7e86f28c4cc1b78db0d342037cdfd3abbdf372ae
chris097 <chrisfidel.international@gmail.com>
2020-11-23 07:06:42 +0100

    middleware error-handling

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+require('express-async-errors')
+const error = require('./middleware/error')
+require('express-async-errors')
 const mongoose = require('mongoose');
 const helmet = require('helmet')
 const items = require('./routes/items')
 const express = require("express");
 const app = express();
-app.use(express.json())
-app.use(helmet())
-app.use('/api/items', items)
-
+//Connected to MongoDB
 mongoose.connect('mongodb://localhost/lib-shelter')
 .then(() => console.log('Connected to MongoDB...'))
 .catch(err => console.error('Could not connect to MongoDB..', err))
+//Midddleware
+app.use(express.json())
+app.use(helmet())
+app.use('/api/items', items)
+app.use(error)
 
 console.log(app.get('env'))
 
